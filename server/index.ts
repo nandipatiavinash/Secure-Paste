74fe3d33e75ad77e42384f737137b9eaa1b390fb
Avinash Chowdary Nandipati <113230925+nandipatiavinash@users.noreply.github.com>
2025-08-31 23:25:32 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -44,9 +44,9 @@ app.use((req, res, next) => {
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "secure-paste-six.vercel.app", // local dev
-      "https://secure-paste-git-main-nandipatiavinash19-1566s-projects.vercel.app",      // your Vercel frontend domain
+      "http://localhost:5173",                // local dev
+      /\.vercel\.app$/, 
+      //"https://secure-paste-git-main-nandipatiavinash19-1566s-projects.vercel.app",      // your Vercel frontend domain
     ],
     credentials: true, // allow cookies/sessions
   })
