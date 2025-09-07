798d8128fbec9b500c4f759e73cc96883a99a0ab
Avinash Chowdary Nandipati <113230925+nandipatiavinash@users.noreply.github.com>
2025-09-08 00:37:11 +0530

    Update schema.ts

--- shared/schema.ts
+++ shared/schema.ts
@@ -7,7 +7,6 @@ import { z } from "zod";
 export const users = pgTable("users", {
   id: varchar("id").primaryKey().default(sql`gen_random_uuid()`),
   email: text("email").notNull().unique(),
-  password: text("password").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
